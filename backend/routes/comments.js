6bd7401b6499cbfcdf0fa0022915e1ab0e145ab4
Dũng Bùi <buimanhdung3007@gmail.com>
2025-12-23 12:45:08 +0700

    update JP for admin

--- backend/routes/comments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/auth.js'); // Import middleware xác thực thật
-const { getCommentsByFoodId, addComment } = require('../controllers/commentController');
-
-router.get('/:foodId', getCommentsByFoodId);
-router.post('/:foodId', authMiddleware, addComment); // Sử dụng middleware thật
-
-module.exports = router;
